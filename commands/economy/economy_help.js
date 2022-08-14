a4be1e096c4d407477c8133c06f0a90cba352234
PAW122 <70094237+PAW122@users.noreply.github.com>
2022-08-14 14:09:06 +0200

    data

--- commands/economy/economy_help.js
+++ commands/economy/economy_help.js
@@ -21,7 +21,9 @@ module.exports = {
     .addFields(//inline-w embedzie zamiast po dobą będą obok siebie
         { name: `Economy version: ${economy_db_version}`, value: `jeżeli twoja wersja wsystemu ekonomi jest starsza użyj $ecodeafult -- przywróci to system do deafultowych ustawień, statystki wszystkich urzytkowników zostaną zrestartowane, ale uzyskasz dostęp do nowych funkcji`, inline: true },
         { name: `$daily`, value: `odbierz dzienną nagrode od 50 do 100 ${emoji}`, inline: true },
+        { name: `$birthday`, value: `odbierz przeznt urodzinowy`, inline: true },
         { name: `$profil`, value: `wyświetla informacje ile posiadasz ${emoji} itp`, inline: true },
+        { name: `$weekly`, value: `odbierz tygondniową nagrodę${emoji}`, inline: true },
         { name: `$roll`, value: `masz 30% szans na podwojenie ${emoji}. Maxymalnie można użyć 15 razy dziennie\n użycie: $roll 10`, inline: true },
         { name: `$shop`, value: `wyświetla liste przedmiotów, które można kupićc`, inline: true },
         { name: `$add`, value: `pozwala administracji dodawać i odejmować ${emoji} urzytkownikom
