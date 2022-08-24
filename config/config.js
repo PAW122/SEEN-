bdd6ac2448dae5597dc0c2f7c948ab66cb1b7838
PAW122 <70094237+PAW122@users.noreply.github.com>
2022-08-24 21:15:03 +0200

    fix token

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //token for tests
-    token: "ODY5NTg3ODc3NDc3MTAxNTkw.GlzDM1.-g1GK0ESq9eZOp92S77v_oo6-SquxCiBLFetaY",
+    token: "ODY5NTg3ODc3NDc3MTAxNTkw.GIyN9g.NpipCONWmTPmMR8Qfz09xXgfX6HH1o67_L9HZ0",
     test_bot: false,
     prefix: "$",//set deafult prefix
     commands_logs: "True",//True/False
