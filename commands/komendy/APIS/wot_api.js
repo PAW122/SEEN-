0310407d54957f89b22dd2e36a2bc3c369a3b2ce
PAW122 <70094237+PAW122@users.noreply.github.com>
2022-11-19 00:35:22 +0100

    wot command

---
+++ commands/komendy/APIS/wot_api.js
@@ -0,0 +1,90 @@
+const Discord = require("discord.js");
+const axios = require("axios");
+
+const config = require("../../../config/config")
+const API_KEY = config.wargaming_api_key
+
+module.exports = {
+    name: "wot",
+
+    execute:async(message,args,client) => {
+        if(args[0] == "help") {
+            const embed_pl = new Discord.MessageEmbed()
+
+                .setColor(`BLUE`)//PL
+                .setTitle("World of thanks Stats")
+                .setFields(
+                    {name: "Nickname:",value: `nick`}
+                )
+        }
+        if(!args[0]) return message.reply("type nickname")
+        const nickname = args[0]
+        const link = `https://api.worldoftanks.eu/wot/account/list/?application_id=${API_KEY}&search=${nickname}`
+        const res = await axios.get(link).catch(err => {
+            console.log(err)
+        })
+        //console.log(res.data)
+        try{
+        //if(res.data.status == "ok") return message.reply("error")
+        if(res.data.status == 'error' && res.data.error.field == "search") return message.reply("Incorrect Nickname")
+        }catch(err){
+            console.log(err)
+        }
+        const nick = res.data.data[0].nickname
+        const account_id = res.data.data[0].account_id
+
+        const link2 = `https://api.worldoftanks.eu/wot/account/info/?application_id=${API_KEY}&account_id=${account_id}`
+        const res2 = await axios.get(link2).catch(err => {
+            console.log(err)
+        })
+
+        //console.log(res2.data)
+
+        const last_battle_time = res2.data.data[account_id].last_battle_time
+        const created_at = res2.data.data[account_id].created_at
+        const global_rating = res2.data.data[account_id].global_rating
+        const logout_at = res2.data.data[account_id].logout_at
+
+        //statistic
+        const stats = res2.data.data[account_id].statistics
+        console.log(stats)
+
+        const all_stats = stats.all
+        const spotted = all_stats.spotted
+        const max_xp = all_stats.max_xp
+        const avg_damage_blocked = all_stats.avg_damage_blocked
+        const hits_percents = all_stats.hits_percents
+        const battles = all_stats.battles
+
+        //staty do coliczenia
+
+        //wr
+        const all_battles = all_stats.battles
+        const draws = all_stats.draws
+        const wins = all_stats.wins
+        const losses = all_stats.losses
+        const wr = (wins/(all_battles))*100
+        console.log(`wr: ${wr}`)
+
+        //srv rate
+        const survived_battles = all_stats.survived_battles
+        const srv_ratio = (survived_battles/all_battles)*100
+        console.log(`srv ratio: ${srv_ratio}`)
+
+        //kpb
+        const frags = all_stats.frags
+        const kpb = (frags/all_battles)
+        console.log(`kils per batle: ${kpb}`)
+
+        //max damage tank
+        return console.log("nie wiem jak ogarnąć nazwe czołgu po id")
+        const max_damage = all_stats.max_damage
+        const max_damage_tank_id = all_stats.max_damage_tank_id
+        const link3 = `https://api.worldoftanks.eu/wot/account/tanks/?application_id=${API_KEY}&account_id=${account_id}&fields=tank_id&tank_id=${max_damage_tank_id}`
+        const res3 = await axios.get(link3).catch(err => {
+            console.log(err)
+        })
+
+        console.log(res3.data.data[account_id])
+    }
+}
