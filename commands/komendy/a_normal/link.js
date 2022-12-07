8f7e11807215d886f2eaa7198e73931a8b56f2de
PAW122 <70094237+PAW122@users.noreply.github.com>
2022-12-07 22:24:27 +0100

    add slash

--- commands/komendy/a_normal/link.js
+++ commands/komendy/a_normal/link.js
@@ -1,12 +1,19 @@
-
+const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     name: "link",
+    isSlash: true,
+
+    data: new SlashCommandBuilder()
+        .setName('link')
+        .setDescription('Send link to bot'),
+    executeInteraction: async (inter) => {
+        return inter.reply("** Invite bot link:** \n https://discord.com/api/oauth2/authorize?client_id=797070806885990431&permissions=8&scope=bot%20applications.commands")
+    },
 
     execute: async(message,args,client) => {
         if(args[0] == "help") {
             return message.reply("user **$link** to get invite bot link")
         }
-
         return message.reply("** Invite bot link:** \n https://discord.com/api/oauth2/authorize?client_id=797070806885990431&permissions=8&scope=bot%20applications.commands")
     }
 }
