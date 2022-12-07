705414dcbf4cd7c00e205101d04c722e3b43c47c
PAW122 <70094237+PAW122@users.noreply.github.com>
2022-12-07 22:24:19 +0100

    add slash to bruh command

--- commands/komendy/a_normal/bruh.js
+++ commands/komendy/a_normal/bruh.js
@@ -1,6 +1,17 @@
 const { MessageAttachment } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 module.exports = {
     name: "bruh",
+    isSlash: true,
+
+    data: new SlashCommandBuilder()
+        .setName('bruh')
+        .setDescription('Send bruh image'),
+    executeInteraction: async (inter) => {
+        const attachment = new MessageAttachment(`db/images/bruh.webp`)
+       return inter.channel.send({files: [attachment]});
+    },
     
     execute: async(message,args,client) => {
         const attachment = new MessageAttachment(`db/images/bruh.webp`)
