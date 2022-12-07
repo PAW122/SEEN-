f21f616400062714e8e0861994bc3d67a69b00e0
PAW122 <70094237+PAW122@users.noreply.github.com>
2022-12-07 22:23:34 +0100

    add slash to mute command

--- commands/komendy/a_normal/mute.js
+++ commands/komendy/a_normal/mute.js
@@ -7,6 +7,81 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     name: "mute",
+    isSlash: true,
+
+    data: new SlashCommandBuilder()
+        .setName('mute')
+        .setDescription('Mute user')
+
+        .addUserOption((option) => option
+            .setName('user')
+            .setDescription('User to mute')
+            .setRequired(true))
+
+            .addNumberOption((option) => option
+            .setName('time')
+            .setDescription('Mute time in minutes')
+            .setRequired(true)),
+
+    executeInteraction: async (inter,client) => {
+        const guildId = inter.guild.id
+        const db2 = new QuickDB({ filePath: process.cwd() + `/db/srv_settings/commands/${guildId}.sqlite` });
+        if (await db2.get(`check.check`) == true) {
+            const settings = await db2.get(`mute.worker`)
+            const settings_reason = await db2.get(`mute.reason`)
+            if (settings == false) { return message.reply(settings_reason) }
+        }
+
+        if (!inter.member.permissions.has(FLAGS.ADMINISTRATOR )) {
+            return(inter.reply('Nie masz wystarczających permisji aby użyć tej komendy!'))
+        } 
+        //dodać logi kto i kogo mutował
+
+        //pierwsza zapingowana osoba
+        const member = inter.options.getUser('user');
+        let toMute = inter.guild.members.cache.find(mem => mem.id === member.id)
+        const muteTime = inter.options.getNumber('time') + "m"
+        let muteRole = inter.guild.roles.cache.find(r => r.name === 'Muted');
+
+        if (!toMute) return inter.reply("you have not provided information about whom you want to mute");
+
+        if (!muteRole) {
+            try {
+                muteRole = await inter.guild.roles.create({
+                    name: "Muted",
+                    color: "#000000",
+                    permissions: []
+                });
+
+
+                inter.guild.channels.cache.forEach(async channel => {
+                    channel.permissionOverwrites.edit(muteRole, {
+                        SEND_MESSAGES: false
+                    })
+                });
+
+
+
+            } catch (err) {
+                console.log(err)
+            }
+
+        }
+
+        if (!muteTime) return inter.reply(`You dont type on how many time u link mute ${toMute}`)
+
+        //dodajemy urzytkownikowi role
+        await (toMute.roles.add(muteRole.id))
+
+        inter.reply(`Sucesfully muted ${toMute.user.tag} no next ${muteTime}`)
+
+        
+
+        setTimeout(function () {
+            toMute.roles.remove(muteRole.id);
+            inter.channel.send(`${toMute} has been unmuted`)
+        }, ms(muteTime))
+    },
 
     execute: async (message, args, client) => {
 
@@ -76,6 +151,7 @@ module.exports = {
             toMute.roles.remove(muteRole.id);
             message.channel.send(`${toMute} has been unmuted`)
         }, ms(muteTime))
+        
     }
 
 }
