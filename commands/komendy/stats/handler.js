5bbdf79753816faa9f2adc7a8ceac76a49968c30
PAWIU <70094237+PAW122@users.noreply.github.com>
2022-12-20 10:40:49 +0100

    starts auto refresh handler

---
+++ commands/komendy/stats/handler.js
@@ -0,0 +1,7 @@
+const auto = require("./auto.js")
+forom auto import members
+module.exports = {
+  //dodać if only_ref === true => wyamga tylko id gildii
+  auto(null, true, guildID)
+  //taksamo z resztą unkcji
+}
