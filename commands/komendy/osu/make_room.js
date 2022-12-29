9a94b54238f369b72050d95332ff8d695029f975
PAW122 <70094237+PAW122@users.noreply.github.com>
2022-12-29 01:46:54 +0100

    del osu commands

--- commands/komendy/osu/make_room.js
+++
@@ -1,3 +0,0 @@
-//make autohost room
-//args[1] == starrate
-//args[2] == passord?
--- commands/komendy/stats/auto.js
+++ commands/komendy/stats/auto.js
@@ -2,7 +2,13 @@ const Discord = require("discord.js")
 module.exports = {
     name: "stats-channel",
 
-    execute: async (message, args, client) => {
+    execute: async (message, args, client, ref, server) => {
+
+        if (ref == true) {
+            //auto refresh stats for all servers
+            auto_all(message, true, client, server)
+            return
+        }
 
         //odświerzanie:
         //dodać liste (w ramie jak w cooldownach)
@@ -11,13 +17,14 @@ module.exports = {
         /*
         trzeba to zrobić w handlerze który wyłapuje gildie po ich id bez wymagania wiadomości
         */
-       //to do: $$stats-channel date tworzy nowy kanał! dać if startsWith date-year czy coś
+        //to do: $$stats-channel date tworzy nowy kanał! dać if startsWith date-year czy coś
 
         if (args[0] == "help") {
             const embed_pl = new Discord.MessageEmbed()
                 .setTitle("Server Live Stats")
-                .setDescription(`create voice channel showing server stats\n to refresh stats use **$stats-channel refresh**\n auto refresh been added in future`)
+                .setDescription(`to manually refresh stats use **$stats-channel refresh**\n`)
                 .setFields(
+                    { name: "Refresh system", value: "all statistics channels will be automatically refreshed after sending any message in the text channel. The maximum frequency of updating statistics is 10 minutes \n\n" },
                     { name: "if u wana automatic create all stats channels use", value: "$stats-channel auto" },
                     { name: "show only online users", value: "$stats-channel users" },
                     { name: "show only online users without counting bots", value: "$stats-channel onlyusers" },
@@ -54,14 +61,14 @@ module.exports = {
 
         async function main(message, args, client) {
             if (args[0] == "auto") {
-                auto_all(message, false)
+                auto_all(message, false, client)
             }
             if (args[0] == "members") {
                 members(message, false)
                 message.react("✅")
             }
             if (args[0] == "refresh") {
-                auto_all(message, true)
+                auto_all(message, true, client)
                 message.reply("Done")
             }
             if (args[0] == "onlymembers") {
@@ -96,12 +103,17 @@ module.exports = {
                 date(message, false)
                 message.react("✅")
             }
-            if(args[0] == "last-join") {
+            if (args[0] == "last-join") {
                 return message.reply("this feature will be available soon")
             }
         }
 
-        async function date(message, only_ref) {
+        async function date(message, only_ref, ref_guild) {
+            if (only_ref == true) {
+                var guild = ref_guild
+            } else {
+                var guild = message.guild
+            }
             var current = new Date();
             const now_rok = current.getFullYear();
             const now_month = current.getMonth() + 1;
@@ -110,7 +122,7 @@ module.exports = {
             const date = now_rok + " " + now_month + " " + "" + now_day
 
             const channel_name = `date`//zmiana wielkośic litery
-            let members_channel = message.guild.channels.cache
+            let members_channel = guild.channels.cache
             //console.log(members_channel)
             var is_channel = false
             if (only_ref != true) {
@@ -135,18 +147,23 @@ module.exports = {
             members_channel.forEach(channel => {
                 if (channel.name.startsWith(channel_name)) {
                     channel.setName(channel_name + "-" + date)
-                    console.log('channal name change')
+                    //console.log('channal name change')
                     return true;
                 }
             });
         }
 
-        async function bans(message, only_ref) {
-            message.guild.bans.fetch()
+        async function bans(message, only_ref, ref_guild) {
+            if (only_ref == true) {
+                var guild = ref_guild
+            } else {
+                var guild = message.guild
+            }
+            guild.bans.fetch()
                 .then(banned => {
                     const bans = banned.size
                     const channel_name = `bans`
-                    let members_channel = message.guild.channels.cache
+                    let members_channel = guild.channels.cache
                     //console.log(members_channel)
                     var is_channel = false
                     if (only_ref != true) {
@@ -171,7 +188,7 @@ module.exports = {
                     members_channel.forEach(channel => {
                         if (channel.name.startsWith(channel_name)) {
                             channel.setName(channel_name + "-" + bans)
-                            console.log('channal name change')
+                            //console.log('channal name change')
                             return true;
                         }
                     });
@@ -179,9 +196,14 @@ module.exports = {
                 })
         }
 
-        async function vc_channels(message, only_ref) {
+        async function vc_channels(message, only_ref, ref_guild) {
+            if (only_ref == true) {
+                var guild = ref_guild
+            } else {
+                var guild = message.guild
+            }
             let i = 0;
-            message.guild.channels.cache.forEach(channel => {
+            guild.channels.cache.forEach(channel => {
                 if (channel.type == "GUILD_VOICE") {
                     i += 1
                 }
@@ -189,7 +211,7 @@ module.exports = {
             const cv_channels = i
 
             const channel_name = `vc-channels`
-            let members_channel = message.guild.channels.cache
+            let members_channel = guild.channels.cache
             //console.log(members_channel)
             var is_channel = false
             if (only_ref != true) {
@@ -214,15 +236,20 @@ module.exports = {
             members_channel.forEach(channel => {
                 if (channel.name.startsWith(channel_name)) {
                     channel.setName(channel_name + "-" + cv_channels)
-                    console.log('channal name change')
+                    //console.log('channal name change')
                     return true;
                 }
             });
         }
 
-        async function txt_channels(message, only_ref) {
+        async function txt_channels(message, only_ref, ref_guild) {
+            if (only_ref == true) {
+                var guild = ref_guild
+            } else {
+                var guild = message.guild
+            }
             let i = 0;
-            message.guild.channels.cache.forEach(channel => {
+            guild.channels.cache.forEach(channel => {
                 if (channel.type == "GUILD_TEXT") {
                     i += 1
                 }
@@ -230,7 +257,7 @@ module.exports = {
             const txt_channels = i
 
             const channel_name = `txt-channels`
-            let members_channel = message.guild.channels.cache
+            let members_channel = guild.channels.cache
             //console.log(members_channel)
             var is_channel = false
             if (only_ref != true) {
@@ -255,17 +282,20 @@ module.exports = {
             members_channel.forEach(channel => {
                 if (channel.name.startsWith(channel_name)) {
                     channel.setName(channel_name + "-" + txt_channels)
-                    console.log('channal name change')
+                    //console.log('channal name change')
                     return true;
                 }
             });
         }
 
-        async function only_bots(message, only_ref) {
-            //zrobić to na for each user if user == bot i += 1 
-
+        async function only_bots(message, only_ref, ref_guild) {
+            if (only_ref == true) {
+                var guild = ref_guild
+            } else {
+                var guild = message.guild
+            }
             let i = 1;
-            message.guild.members.fetch().then(members => {
+            guild.members.fetch().then(members => {
                 members.forEach(member => {
                     if (member.user.bot == true) {
                         i += 1
@@ -274,7 +304,7 @@ module.exports = {
                 const guild_bots = i
 
                 const channel_name = `bots`
-                let members_channel = message.guild.channels.cache
+                let members_channel = guild.channels.cache
                 //console.log(members_channel)
                 var is_channel = false
                 if (only_ref != true) {
@@ -299,7 +329,7 @@ module.exports = {
                 members_channel.forEach(channel => {
                     if (channel.name.startsWith(channel_name)) {
                         channel.setName(channel_name + "-" + guild_bots)
-                        console.log('channal name change')
+                        //console.log('channal name change')
                         return true;
                     }
                 });
@@ -307,17 +337,22 @@ module.exports = {
 
         }
 
-        async function only_members(message, only_ref) {
+        async function only_members(message, only_ref, ref_guild) {
+            if (only_ref == true) {
+                var guild = ref_guild
+            } else {
+                var guild = message.guild
+            }
             var i = 0;
-            message.guild.members.fetch().then(members => {
+            guild.members.fetch().then(members => {
                 members.forEach(member => {
                     if (member.user.bot == false) {
                         i += 1
                     }
                 });
                 const users = i
-                const channel_name = `only-members`
-                let members_channel = message.guild.channels.cache
+                const channel_name = `users`
+                let members_channel = guild.channels.cache
                 //console.log(members_channel)
                 var is_channel = false
                 if (only_ref != true) {
@@ -342,17 +377,22 @@ module.exports = {
                 members_channel.forEach(channel => {
                     if (channel.name.startsWith(channel_name)) {
                         channel.setName(channel_name + "-" + users)
-                        console.log('channal name change')
+                        //console.log('channal name change')
                         return true;
                     }
                 });
             });
         }
 
-        async function members(message, only_ref) {
-            const members = message.guild.memberCount
+        async function members(message, only_ref, ref_guild) {
+            if (only_ref == true) {
+                var guild = ref_guild
+            } else {
+                var guild = message.guild
+            }
+            const members = guild.memberCount
             const channel_name = `members`
-            let members_channel = message.guild.channels.cache
+            let members_channel = guild.channels.cache
             //console.log(members_channel)
             var is_channel = false
             if (only_ref != true) {
@@ -377,20 +417,25 @@ module.exports = {
             members_channel.forEach(channel => {
                 if (channel.name.startsWith(channel_name)) {
                     channel.setName(channel_name + "-" + members)
-                    console.log('channal name change')
+                    //console.log('channal name change')
                     return true;
                 }
             });
         }
 
-        async function roles(message, only_ref) {
+        async function roles(message, only_ref, ref_guild) {
+            if (only_ref == true) {
+                var guild = ref_guild
+            } else {
+                var guild = message.guild
+            }
             let i = 0;
-            message.guild.roles.cache.forEach(role => {
+            guild.roles.cache.forEach(role => {
                 i += 1
             })
             const roles = i
             const channel_name = `roles`
-            let members_channel = message.guild.channels.cache
+            let members_channel = guild.channels.cache
             //console.log(members_channel)
             var is_channel = false
             if (only_ref != true) {
@@ -415,21 +460,26 @@ module.exports = {
             members_channel.forEach(channel => {
                 if (channel.name.startsWith(channel_name)) {
                     channel.setName(channel_name + "-" + roles)
-                    console.log('channal name change')
+                    //console.log('channal name change')
                     return true;
                 }
             });
 
         }
 
-        async function channels(message, only_ref) {
+        async function channels(message, only_ref, ref_guild) {
+            if (only_ref == true) {
+                var guild = ref_guild
+            } else {
+                var guild = message.guild
+            }
             let i = 0;
-            message.guild.channels.cache.forEach(channel => {
+            guild.channels.cache.forEach(channel => {
                 i += 1
             })
             const channels = i
             const channel_name = `channels`
-            let members_channel = message.guild.channels.cache
+            let members_channel = guild.channels.cache
             //console.log(members_channel)
             var is_channel = false
             if (only_ref != true) {
@@ -439,11 +489,11 @@ module.exports = {
                     }
                 });
                 if (is_channel == false && only_ref != true) {
-                    message.guild.channels.create(channel_name + "-" + channels, {
+                    guild.channels.create(channel_name + "-" + channels, {
                         type: "voice",
                         permissionOverwrites: [
                             {
-                                id: message.guild.roles.everyone,
+                                id: guild.roles.everyone,
                                 allow: ['VIEW_CHANNEL'],
                                 deny: ['SEND_MESSAGES', 'READ_MESSAGE_HISTORY']
                             }
@@ -454,25 +504,37 @@ module.exports = {
             members_channel.forEach(channel => {
                 if (channel.name.startsWith(channel_name)) {
                     channel.setName(channel_name + "-" + channels)
-                    console.log('channal name change')
+                    //console.log('channal name change')
                     return true;
                 }
             });
 
         }
 
-        async function auto_all(message, only_ref) {
-            members(message, only_ref)
-            only_members(message, only_ref)
-            only_bots(message, only_ref)
-            roles(message, only_ref)
-            channels(message, only_ref)
-            vc_channels(message, only_ref)
-            txt_channels(message, only_ref)
-            bans(message, only_ref)
-            date(message, only_ref)
-            message.react("✅")
-        }
+        async function auto_all(message, only_ref, client, server) {
+            //server = string with guild id from list with all server id to refresh
+            if (only_ref == true && server) {
+                var ref_guild = await client.guilds.cache.get(server);
+                if (!ref_guild || ref_guild == null || ref_guild == undefined) {
+                    return console.log("stats channel ERROR ref_guild == undefind\n line 519\n auto.js")
+                }
+                ref(message, only_ref, ref_guild)
+            } else if (message) {
+                const server_id = message.guild.id
+                ref(message, only_ref, server_id)
+            }
 
+            function ref(message, only_ref, ref_guild) {
+                channels(message, only_ref, ref_guild)
+                members(message, only_ref, ref_guild)
+                only_members(message, only_ref, ref_guild)
+                only_bots(message, only_ref, ref_guild)
+                roles(message, only_ref, ref_guild)
+                vc_channels(message, only_ref, ref_guild)
+                txt_channels(message, only_ref, ref_guild)
+                bans(message, only_ref, ref_guild)
+                date(message, only_ref, ref_guild)
+            }
+        }
     }
 }
