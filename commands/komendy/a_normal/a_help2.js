f089992cfb87f8ac94545d8dd822b7c2fce60c46
PAW122 <70094237+PAW122@users.noreply.github.com>
2023-01-27 05:19:34 +0100

    del fotter

--- commands/komendy/a_normal/a_help2.js
+++ commands/komendy/a_normal/a_help2.js
@@ -9,41 +9,62 @@ const prefix = config.prefix
 const embed_pl = new Discord.MessageEmbed()
     .setColor(`BLUE`)
     .setTitle(`Help Page`)
-    .setDescription(`use **$help commandname** to get for a more detailed description`)
-    .setFooter("use $report if u fing some errors")
+    .setDescription(`use **$help commandname** to get for a more detailed description\nuse $report if u fing some errors`)
     .addFields(
-        { name: `***😁fun***`, value: 
-        "``ruletka``" +","+ "``bruh``" +","+ "``random``"+","+ "``8ball``" +","+"``flags``"+","+"``animegif``"+","+"``przemyślenia``"+ "," + "``random`` , ``avatar-generator``", inline: false },
+        {
+            name: `***😁fun***`, value:
+                "``ruletka``" + "," + "``bruh``" + "," + "``random``" + "," + "``8ball``" + "," + "``flags``" + "," + "``animegif``" + "," + "``przemyślenia``" + "," + "``random`` , ``avatar-generator``", inline: false
+        },
 
-        { name: `***❓info***`, value: 
-        "``awatar``"+ ","+"``ping``"+","+"``botinfo``"+","+"``srvinfo``"+","+"``lvl``"+","+"``link``"+","+"``isalt`` , ``server``", inline: false },
+        {
+            name: `***❓info***`, value:
+                "``awatar``" + "," + "``ping``" + "," + "``botinfo``" + "," + "``srvinfo``" + "," + "``lvl``" + "," + "``link``" + "," + "``isalt`` , ``server``", inline: false
+        },
 
-        { name: `***👑administration***`, value: 
-        "``clear``"+","+"``say``"+","+"``kick``"+","+"``ban``"+","+"``message_logs``"+","+"``mute``"+","+"``autorole ``", inline: false },
+        {
+            name: `***👑administration***`, value:
+                "``clear``" + "," + "``say``" + "," + "``kick``" + "," + "``ban``" + "," + "``message_logs``" + "," + "``mute``" + "," + "``autorole ``", inline: false
+        },
 
-        { name: `***⚙️bot config***`, value: 
-        "``srv_set ``"+","+"``srv_logs``"+","+"``automod ``" + "," + "``settings auto``" + "," + "``settings owner_alert help`` , ``$stats-channel help``", inline: false },
+        {
+            name: `***⚙️bot config***`, value:
+                "``srv_set ``" + "," + "``srv_logs``" + "," + "``automod ``" + "," + "``settings auto``" + "," + "``settings owner_alert help`` , ``$stats-channel help``", inline: false
+        },
 
-        { name: `***💵economy***`, value: 
-        "``helpeco``"+","+"``daily``"+","+"``profil``"+","+"``weekly``"+","+"``roll``"+","+"``shop``"+","+"``pvp``"+","+"``pay``"+","+"``add``"+","+"``birthday``"+","+"``bank``", inline: false },
+        {
+            name: `***💵economy***`, value:
+                "``helpeco``" + "," + "``daily``" + "," + "``profil``" + "," + "``weekly``" + "," + "``roll``" + "," + "``shop``" + "," + "``pvp``" + "," + "``pay``" + "," + "``add``" + "," + "``birthday``" + "," + "``bank``", inline: false
+        },
 
-        { name: `***🤯reactions***`, value: 
-        "``reactions help``"+","+"``angry``"+","+"``eat``"+","+"``happy``"+","+"``kill``"+","+"``love``"+","+"``run``"+","+"``scarry``"+","+"``sleep``", inline: false },
+        {
+            name: `***🤯reactions***`, value:
+                "``reactions help``" + "," + "``angry``" + "," + "``eat``" + "," + "``happy``" + "," + "``kill``" + "," + "``love``" + "," + "``run``" + "," + "``scarry``" + "," + "``sleep``", inline: false
+        },
 
-        { name: `***🎮Valorant***`, value:
-        "``valo help``" + "," + "``valo mmr``" + "," + "``valo mmr history``" + "," + "``valo get_article``" + "," + "``valo last mathces``" + "," + "``valo agents``" + "," + "``valo player_cards``" + "," + "``valo maps``" + "," + "``version``" + "," + "``valo eavents``", inline: false },
+        {
+            name: `***🎮Valorant***`, value:
+                "``valo help``" + "," + "``valo mmr``" + "," + "``valo mmr history``" + "," + "``valo get_article``" + "," + "``valo last mathces``" + "," + "``valo agents``" + "," + "``valo player_cards``" + "," + "``valo maps``" + "," + "``version``" + "," + "``valo eavents``", inline: false
+        },
 
-        { name: `***🧠Special***`, value:
-        "``mechatronic``,``mp3editor``,``html``,``sens-calculator``,``qr_gen``,``gpt``", inline: false },
+        {
+            name: `***🧠Special***`, value:
+                "``mechatronic``,``mp3editor``,``html``,``sens-calculator``,``qr_gen``,``gpt``,``tic-tac-toe``", inline: false
+        },
 
-        { name: `***🌐APIS***`, value:
-        " ``$write`` , ``chess help`` , ``chess profil`` , ``chess stats``, ``wot``"},
+        {
+            name: `***🌐APIS***`, value:
+                " ``write`` , ``chess help`` , ``chess profil`` , ``chess stats``, ``wot``"
+        },
 
-        { name: `🇯🇵***Anime***`, value:
-        "``animeseen``" + "," + "``anime``", inline: false },
+        {
+            name: `🇯🇵***Anime***`, value:
+                "``animeseen``" + "," + "``anime``", inline: false
+        },
 
-        { name: `***🆘Support***`, value: 
-        "``contact``"+","+"``report``", inline: false }
+        {
+            name: `***🆘Support***`, value:
+                "``contact``" + "," + "``report``", inline: false
+        }
     )
 module.exports = {
     name: `help!`,
@@ -54,25 +75,24 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('send list with all bot commands'),
-    executeInteraction: async (inter) => { 
-        if(work != true){
+    executeInteraction: async (inter) => {
+        if (work != true) {
             const embed_worker = new Discord.MessageEmbed()
-            .setTitle('**help**')
-            .setColor('RANDOM')
-            .setDescription(`${reason}`)
-        inter.reply({ embeds: [embed_worker] });
-        return(console.log("command id disabled"))
-        }else{
-        inter.reply({ embeds: [embed_pl] });
+                .setTitle('**help**')
+                .setColor('RANDOM')
+                .setDescription(`${reason}`)
+            inter.reply({ embeds: [embed_worker] });
+            return (console.log("command id disabled"))
+        } else {
+            inter.reply({ embeds: [embed_pl] });
         }
     },
 
-    execute: async (message, args) => {//trzeba dodać help do anime!!!!
+    execute: async (message, args) => {
 
         if (work != true) { return message.channel.send(reason) }
-            //wersja polska
-            return message.channel.send({ embeds: [embed_pl] });
-        
+        //wersja polska
+        return message.channel.send({ embeds: [embed_pl] })
     }
 
 }
